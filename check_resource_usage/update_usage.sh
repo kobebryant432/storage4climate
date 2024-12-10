23df5d90b318040a8e4b2e00a5e992daaefc3f64
Inne Vanderkelen <inne.vanderkelen@vub.be>
2024-12-10 10:27:02 +0100

    update usage script for all projects

--- check_resource_usage/update_usage.sh
+++ check_resource_usage/update_usage.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-project_name=$1
 
-# If the project name is not provided, use the default project name
-if [ -z "$project_name" ]; then
-    project_name="2022_205"
-fi
+# List of project names to iterate over
+project_names=("2022_201" "2022_202" "2022_203" "2022_204" "2022_205")
 
-#Ensure the script is run from the correct directory
+# Ensure the script is run from the correct directory
 cd "$(dirname "$0")"
 
-# Get the csv file from the project
-file=/dodrio/scratch/projects/$project_name/.resource_app.usage.export.csv
-git_dir=$(git rev-parse --show-toplevel)
-input_file=$git_dir/VSC_monitoring/input/usage_$(date +"%Y%m%d").csv
+# Iterate over each project name
+for project_name in "${project_names[@]}"; do
+    echo "Processing project: $project_name"
 
-cp $file $input_file
+    # Get the CSV file from the project
+    file="/dodrio/scratch/projects/$project_name/.resource_app.usage.export.csv"
+    git_dir=$(git rev-parse --show-toplevel)
+    input_file="$git_dir/VSC_monitoring/input/usage_$(date +"%Y%m%d")_${project_name}.csv"
 
-# Remove all lines before the line containing "Project Usages" but keep the line itself
-sed -i '1,/Project Usages/{/Project Usages/!d}' $input_file
+    # Copy the file
+    cp "$file" "$input_file"
+
+    # Remove all lines before the line containing "Project Usages" but keep the line itself
+    sed -i '1,/Project Usages/{/Project Usages/!d}' "$input_file"
+
+    echo "Copied project: $project_name"
+done
 
 # Add and commit the changes
 git pull
